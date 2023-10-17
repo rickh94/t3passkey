4f0657a6546671743c31bba1b367619199c62d9d
Rick Henry <rickhenry@rickhenry.dev>
2023-10-17 19:38:15 -0400

    add styling, refactor to separate layout component
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -8,6 +8,7 @@ import {
 import { useRouter } from "next/router";
 import directApi from "~/utils/directApi";
 import { startAuthentication } from "@simplewebauthn/browser";
+import { PageLayout } from "~/components/layout";
 
 export default function SignInComponent() {
   const [email, setEmail] = useState("");
@@ -67,24 +68,37 @@ export default function SignInComponent() {
   }
 
   return (
-    <div>
-      <main>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            autoComplete="home email"
-            placeholder="Enter your email"
-            value={email}
-            onChange={updateEmail}
-            onKeyDown={handleKeyDown}
-          />
-          <button disabled={!isValid} onClick={handleSignIn} type="button">
-            Sign in
-          </button>
-        </form>
-      </main>
-    </div>
+    <PageLayout>
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="flex w-full flex-col gap-4 sm:w-72"
+      >
+        <div>
+          <h1 className="text-4xl font-bold text-white">Log In</h1>
+          <p className="py-2 text-white">Type your email to get started</p>
+        </div>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          autoComplete="home email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={updateEmail}
+          onKeyDown={handleKeyDown}
+          className="rounded-full border-slate-200 bg-white/10 px-4 py-2 text-white shadow focus:bg-white/30 focus:outline-none focus:ring-2 focus:ring-white/50 focus:ring-offset-2 focus:ring-offset-[#2e026d]"
+        />
+        <button
+          disabled={!isValid}
+          onClick={handleSignIn}
+          type="button"
+          className={`w-full rounded-full px-10 py-3 font-semibold text-white transition ${isValid ? "bg-white/20 shadow hover:bg-white/20" : "bg-slate-100/20"
+            }`}
+        >
+          Sign in
+        </button>
+      </form>
+    </PageLayout>
   );
 }
+// TODO: restyle this
